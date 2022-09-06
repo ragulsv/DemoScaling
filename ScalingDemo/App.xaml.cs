5e365586ba7fd5f338b7c832bc38bb4df1588cc3
Ragul S V <s.v.ragul98@gmail.com>
2022-09-06 15:53:32 +0530

    Scaling demo sample commit

---
+++ ScalingDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ScalingDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ScalingDemo/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace ScalingDemo;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		image.Scale += 1;
+	}
+}
+
